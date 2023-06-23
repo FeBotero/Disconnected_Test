513475e08a194be8c36083ce51af254fe1b992f7
FeBotero <frpbotero@gmail.com>
2023-06-22 20:50:48 -0400

    First Commit

--- app/src/main/java/com/motoacademy/disconnectedd/MainActivity.java
+++ app/src/main/java/com/motoacademy/disconnectedd/MainActivity.java
@@ -1,12 +1,12 @@
 package com.motoacademy.disconnectedd;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button btnChange ;
